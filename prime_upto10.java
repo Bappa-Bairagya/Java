cc35fd8e68acacd7c672e36545531d41f76b0127
Bappa-Bairagya <bappa.09092003@gmail.com>
2024-12-25 17:56:00 +0530

    add prime upto10

---
+++ prime_upto10.java
@@ -0,0 +1,21 @@
+public class prime_upto10 {
+    static void check(int n){
+        int count=0;
+        //System.out.println(n);
+        for(int i=1;i<n;i++){
+            if (n%i==0) {
+                count++;
+            }
+        }
+        //System.out.println(count);
+        if (count==1) {
+            System.out.println("this numbeer is prime "+n);
+        }
+    }
+    public static void main(String[] args) {
+        int n=10;
+        for(int i=1;i<=n;i++){
+            check(i);
+        }
+    }
+}
