2398f3006e53f1333dfdb9a725ed5af02887c256
Bappa-Bairagya <bappa.09092003@gmail.com>
2024-12-25 16:58:05 +0530

    add check prime

---
+++ check_prime.java
@@ -0,0 +1,21 @@
+public class check_prime {
+    static void check(int n){
+        int count=0;
+        //System.out.println(n);
+        for(int i=1;i<n;i++){
+            if (n%i==0) {
+                count++;
+            }
+        }
+        System.out.println(count);
+        if (count==1) {
+            System.out.println("this numbeer is prime");
+        }
+        else{
+            System.out.println("this is not prime");
+        }
+    }
+    public static void main(String[] args) {
+        check(11);
+    }
+}
