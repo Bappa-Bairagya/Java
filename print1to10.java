94961b4e10b138a3fdc3fb3ce9d229d35babe6e2
Bappa-Bairagya <bappa.09092003@gmail.com>
2024-12-26 21:57:21 +0530

    add with *

--- print1to10.java
+++ print1to10.java
@@ -1,7 +1,10 @@
 public class print1to10 {
     public static void main(String[] args) {
         for(int i=1;i<=10;i++){
-            System.out.println(i);
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println(" ="+i);
         }
     }
 }
