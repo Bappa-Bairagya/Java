33da4d1ae77df31bfe4c3aef5de22377a24f346b
Bappa-Bairagya <bappa.09092003@gmail.com>
2024-12-26 22:01:28 +0530

    add with star pattarn

---
+++ basic_star_pattern.java
@@ -0,0 +1,10 @@
+public class basic_star_pattern {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+        System.out.println("*****");
+        //System.out.println("OK !! THIS IS ONLY FOR FUN");
+    }
+}
