31f9695b9f3723cb66615d986ce609c96f73dc62
Bappa-Bairagya <bappa.09092003@gmail.com>
2024-12-28 21:49:10 +0530

    add fibonacci

---
+++ fibonacci.java
@@ -0,0 +1,15 @@
+public class fibonacci {
+    static void operation(int n){
+        int f1=0;
+        int f2=1;
+        for(int i=1;i<=n;i++){
+            int f3=f1+f2;
+            System.out.println(f3);
+            f1=f2;
+            f2=f3;
+        }
+    }
+    public static void main(String[] args) {
+        operation(5);
+    }
+}
