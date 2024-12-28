9f1988b65a8fcf8eced1650054a5d22c0f6780b1
Bappa-Bairagya <bappa.09092003@gmail.com>
2024-12-28 21:50:35 +0530

    add fibonacci

--- fibonacci.java
+++ fibonacci.java
@@ -3,6 +3,7 @@ public class fibonacci {
         int f1=0;
         int f2=1;
         System.out.println(f1);
+        System.out.println(f2);
         for(int i=1;i<=n;i++){
             int f3=f1+f2;
             System.out.println(f3);
