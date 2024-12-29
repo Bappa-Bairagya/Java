a624a73f4403eeda240721db652e600c6a74d7ca
Bappa-Bairagya <bappa.09092003@gmail.com>
2024-12-29 23:07:50 +0530

    add palindrum

--- pallindrum.java
+++ pallindrum.java
@@ -16,7 +16,7 @@ public class pallindrum {
     }
     public static void main(String[] args) {
         new pallindrum().operation(151);
-        //System.out.println("hiiiiiiiiiiiii");
+        
         
     }
 }
