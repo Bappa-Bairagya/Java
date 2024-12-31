203878b75fce558a96f85c713bc5fe27ca799053
Bappa-Bairagya <bappa.09092003@gmail.com>
2024-12-31 10:00:54 +0530

    add 2d array

--- array_all.java
+++ array_all.java
@@ -15,8 +15,9 @@ public class array_all {
         System.out.println("this is 2d array");
         for(int i=0;i<3;i++){
             for(int j=0;j<3;j++){
-                System.out.println("the array is "+b[i][j]);
+                System.out.print(b[i][j]+" ");
             }
+            System.out.println();
         }
         
     }
