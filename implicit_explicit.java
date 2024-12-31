3d99fe689096a2eb3fa2f39d2f4a682d266186d0
Bappa-Bairagya <bappa.09092003@gmail.com>
2024-12-31 09:14:48 +0530

    add implicit converstion

---
+++ implicit_explicit.java
@@ -0,0 +1,9 @@
+public class implicit_explicit {
+    public static void main(String[] args) {
+        System.out.println("here it's implicit");
+        int n=40;
+        long l=n;
+        float f=l;
+        System.out.println(f);
+    }
+}
