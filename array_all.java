a26ebab02c6e7a1dbc45a99f402ba5cfcd317b70
Bappa-Bairagya <bappa.09092003@gmail.com>
2024-12-31 09:59:22 +0530

    add 2d array

--- array_all.java
+++ array_all.java
@@ -11,5 +11,13 @@ public class array_all {
         for(int i=0;i<a.length;i++){
             System.out.println("the array is "+a[i]);
         }
+        int [][]b={{4,5,6},{7,8,9},{1,2,3}};
+        System.out.println("this is 2d array");
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                System.out.println("the array is "+b[i][j]);
+            }
+        }
+        
     }
 }
