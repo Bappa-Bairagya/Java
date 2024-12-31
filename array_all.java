5ac71d3973c35909320d3c5b1c5f1288087e73cc
Bappa-Bairagya <bappa.09092003@gmail.com>
2024-12-31 09:31:57 +0530

    add 1d arrary

---
+++ array_all.java
@@ -0,0 +1,12 @@
+public class array_all {
+    public static void main(String[] args) {
+        int []a=new int[5];
+        a[0]=5;
+        a[1]=6;
+        a[2]=4;
+        a[3]=9;
+        a[4]=10;
+        System.out.println(a[4]);
+        System.out.println("this is 1d array");
+    }
+}
