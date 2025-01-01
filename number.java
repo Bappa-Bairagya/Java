5b7399a77a19e74540ec3f9169444f3282a33dff
Bappa-Bairagya <bappa.09092003@gmail.com>
2025-01-01 23:44:53 +0530

    add new number

--- number.java
+++ number.java
@@ -1,6 +1,7 @@
 public class number {
     public static void main(String[] args) {
         int n=10;
+        System.out.println("normal print for number 1-10");
         for(int i=1;i<=n;i++){
             System.out.println(i);
         }
