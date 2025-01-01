e1fa7d7f110b14751bb920c1691c8a8346d04388
Bappa-Bairagya <bappa.09092003@gmail.com>
2025-01-01 23:48:36 +0530

    add new line

--- print1to10.java
+++ print1to10.java
@@ -1,5 +1,6 @@
 public class print1to10 {
     public static void main(String[] args) {
+        System.out.println("just a normal pogram");
         for(int i=1;i<=10;i++){
             for(int j=1;j<=i;j++){
                 System.out.print("*");
