ada4ee4bfaec32c74021b1af40ad0a8baccd0353
Bappa-Bairagya <bappa.09092003@gmail.com>
2025-01-02 00:41:57 +0530

    add while loop

--- loop.java
+++ loop.java
@@ -1,8 +1,14 @@
 public class loop {
     public static void main(String[] args) {
         //for loop
-        for(int i=1;i<+10;i++){
+        for(int i=1;i<=10;i++){
             System.out.println(i);
+        }
+        //while loop
+        int i=5;
+        while (i!=0) {
+            System.out.println(i);
+            i=i-1;
         }
     }
 }
