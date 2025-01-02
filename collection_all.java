c3dfd06e4f12d5e03eb0598c62cdeee4989444f9
Bappa-Bairagya <bappa.09092003@gmail.com>
2025-01-02 11:00:12 +0530

    add linkedlist

--- collection_all.java
+++ collection_all.java
@@ -1,7 +1,9 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class collection_all {
     public static void main(String[] args) {
+        //Arraylist
         ArrayList<Integer> arraylist = new ArrayList<Integer>();
         for(int i=1;i<=5;i++){
             arraylist.add(i);
@@ -12,6 +14,20 @@ public class collection_all {
         for(int i=0;i<arraylist.size();i++){
             System.out.println(arraylist.get(i));
         }
+        for (Integer i : arraylist) {
+            System.out.println(i);
+        }
+        //linkedlist
+        LinkedList <String> list = new LinkedList <String>();
+        list.add("A");
+        list.add("B");
+        list.add("C");
+        list.add("D");
+        list.addFirst("L");
+        list.addLast("Z");
+        list.add(5, "M");
+        list.add("n");
+        System.out.println(list);
         
     }
 }
