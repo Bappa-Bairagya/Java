6a9a236a358eed0520cfa7befcf2f1c523965337
Bappa-Bairagya <bappa.09092003@gmail.com>
2025-01-04 02:16:46 +0530

    add inheritence

--- oops_inheritence.java
+++ oops_inheritence.java
@@ -1,5 +1,17 @@
+class  child{
+    int otp=789;
+    
+}//7797451019
+class parent extends child{
+    void submit(){
+        System.out.println("good");
+    }
+}
+
 public class oops_inheritence {
     public static void main(String[] args) {
-        
+        parent p=new parent();
+        p.submit();
+        System.out.println(p.otp);
     }
 }
