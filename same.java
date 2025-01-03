d7ded54c2840645ea41330df90138917c3703c9e
Bappa-Bairagya <bappa.09092003@gmail.com>
2025-01-04 01:52:35 +0530

    add constractor

--- same.java
+++ same.java
@@ -1,5 +1,15 @@
 public class same {
+    int a;
+    int b;
+    same(){
+           this.a=5;
+           this.b=6; 
+    }
+    int value(){
+        return a+b;
+    }
     public static void main(String[] args) {
-        
+        same foo = new same();
+        System.out.println(foo.value());
     }
 }
