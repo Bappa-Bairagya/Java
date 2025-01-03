73af4dc6e715ca42b49c0eef141fca37ea5eaedb
Bappa-Bairagya <bappa.09092003@gmail.com>
2025-01-04 00:42:44 +0530

    add stack

--- stacks.java
+++ stacks.java
@@ -1,5 +1,15 @@
+import java.util.Stack;
+
 public class stacks {
     public static void main(String[] args) {
-        
+        Stack<String> stack = new Stack<>();
+        stack.push("India");
+        stack.push("America");
+        stack.push("Russia");
+        stack.push("China");
+        System.out.println("stack -"+stack);
+        String s=stack.pop();
+        System.out.println(s);
+        System.out.println(stack.peek());
     }
 }
