313ae678eeb43054481a7c0b82afd5fe400412f5
Bappa-Bairagya <bappa.09092003@gmail.com>
2025-01-04 01:26:17 +0530

    add queue

--- queue.java
+++ queue.java
@@ -1,5 +1,16 @@
+import java.util.PriorityQueue;
+import java.util.Queue;
+
 public class queue {
     public static void main(String[] args) {
-        
+        Queue<String> foo = new PriorityQueue<>();
+        foo.add("B");
+        foo.add("C");
+        foo.add("A");
+        foo.add("z");
+        System.out.println(foo);
+        System.out.println(foo.peek());
+        foo.poll();
+        System.out.println(foo);
     }
 }
