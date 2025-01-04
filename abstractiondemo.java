d49f7e1c803917b3a1a3bce75e60e4c7e7e87918
Bappa-Bairagya <bappa.09092003@gmail.com>
2025-01-04 21:11:47 +0530

    add abstarct

---
+++ abstractiondemo.java
@@ -0,0 +1,35 @@
+abstract class value {
+    int i=5;
+    abstract void call();
+    abstract void take();
+}
+class circle extends value{
+    void call(){
+        System.out.println("oops this is circle");
+        System.out.println(i);
+    }
+    
+    void take(){
+        System.out.println("oops this is take circle");
+    }
+}
+class squre extends value{
+    void call(){
+        System.out.println("squre");
+        System.out.println(i);
+    }
+    void take(){
+        System.out.println("new squre");
+    }
+}
+
+public class abstractiondemo {
+    public static void main(String[] args) {
+        value c =new circle();
+        c.call();
+        c.take();
+        value v=new squre();
+        v.call();
+        v.take();
+    }
+}
