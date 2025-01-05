939cc3bf6694c0dbad67948ee5f66e6f884556e1
Bappa-Bairagya <bappa.09092003@gmail.com>
2025-01-05 23:52:33 +0530

    add method over riding

--- load.java
+++ load.java
@@ -1,4 +1,5 @@
 public class load {
+    //method over loading
     static void call(){
         System.out.println("call 1");
     }
---
+++ ridd.java
@@ -0,0 +1,5 @@
+public class ridd {
+    public static void main(String[] args) {
+        
+    }
+}
