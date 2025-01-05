efaf145ae88ad508954dcfd7826fba8f2c1c01c8
Bappa-Bairagya <bappa.09092003@gmail.com>
2025-01-05 23:04:45 +0530

    add interface

--- interfacedemo.java
+++ interfacedemo.java
@@ -1,5 +1,30 @@
+interface animal {
+    void sound();
+    
+}
+class cat implements animal{
+    public void sound(){
+        System.out.println("mewo meow");
+    }
+}
+class dog implements animal{
+    public void sound(){
+        System.out.println("bhaw bhaw");
+    }
+}
+// class scp{
+//     public static void s(animal a){
+//         a.sound();
+//     }
+// }
+
 public class interfacedemo {
     public static void main(String[] args) {
-        
+        cat c=new cat();
+        //scp.s(c);
+        c.sound();
+        dog d=new dog();
+        //scp.s(d);
+        d.sound();
     }
 }
