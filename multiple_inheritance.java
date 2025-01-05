5a1f3541d6744df810edc091951a5690a78c0f5e
Bappa-Bairagya <bappa.09092003@gmail.com>
2025-01-05 23:41:34 +0530

    add multiple inheritance

--- multiple_inheritance.java
+++ multiple_inheritance.java
@@ -1,5 +1,22 @@
-public class multiple_inheritance {
+interface a{
+    void cll();
+}
+interface b{
+    void beta();
+}
+
+public class multiple_inheritance implements a,b {
+    
+        public void cll(){
+            System.out.println("call");
+        }
+        public void beta(){
+            System.out.println("beta");
+        }
+     
     public static void main(String[] args) {
-        
+        multiple_inheritance c=new multiple_inheritance();
+        c.beta();
+        c.cll();
     }
 }
