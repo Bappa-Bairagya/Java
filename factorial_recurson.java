d72eeea9fdf3892df3490d37c20a0e1eeec299db
Bappa-Bairagya <bappa.09092003@gmail.com>
2025-01-06 00:30:00 +0530

    add factorial by recurstion

---
+++ factorial_recurson.java
@@ -0,0 +1,18 @@
+class calulate{
+    int fact(int n){
+        if (n==1) {
+            return n;
+        }
+        else{
+            return(n*fact(n-1));
+        }
+    }
+}
+
+public class factorial_recurson {
+    public static void main(String[] args) {
+        calulate cal=new calulate();
+        int result=cal.fact(10);
+        System.out.println("Factorial is= "+result);
+    }
+}
