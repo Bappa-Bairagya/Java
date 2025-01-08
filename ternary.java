7eba5216215ce5d215a0738083dd10590e5a1c69
Bappa-Bairagya <bappa.09092003@gmail.com>
2025-01-08 22:52:37 +0530

    add normal

--- ternary.java
+++ ternary.java
@@ -1,7 +1,7 @@
 public class ternary {
     public static void main(String[] args) {
         int n=21;
-        String b=(n>18)?"you can vote":"you are kid";
+        String b=(n>18)?"you can vote":"you are not eligible";
         System.out.println(b);
     }
 }
