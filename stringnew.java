414d7adbce8b863c369b982ee40b6b5602f695d4
Bappa-Bairagya <bappa.09092003@gmail.com>
2025-01-10 02:15:33 +0530

    add string with new

---
+++ stringnew.java
@@ -0,0 +1,9 @@
+public class stringnew {
+    public static void main(String[] args) {
+        String str=new String("bappa");
+        
+        String s1=new String("bappa");
+        System.out.println(s1.equals(str));
+        System.out.println(s1==str);
+    }
+}
