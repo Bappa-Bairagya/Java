e8af4722cba4d17e4a7e701d03df0e9cf1814a41
Bappa-Bairagya <bappa.09092003@gmail.com>
2025-01-10 02:24:16 +0530

    add string without new

--- stringnew.java
+++ stringnew.java
@@ -3,7 +3,11 @@ public class stringnew {
         String str=new String("bappa");
         
         String s1=new String("bappa");
-        System.out.println(s1.equals(str));
-        System.out.println(s1==str);
+        System.out.println(s1.equals(str));//it will check value
+        System.out.println(s1==str);//it will check addres
+        String s="hello";
+        String e="hello";
+        System.out.println(s.equals(e));
+        System.out.println(s==e);
     }
 }
