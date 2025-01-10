c665d6cb47a4cbca9455a45b2a4a0587dd698ffb
Bappa-Bairagya <bappa.09092003@gmail.com>
2025-01-10 18:31:09 +0530

    add encapsulation

---
+++ encapsul.java
@@ -0,0 +1,26 @@
+class demo{
+    private String name="bappa";
+    private int age=21;
+    public String getname(){
+        return name;
+    }
+    public int getage(){
+        return age;
+    }
+    public void setage(int n){
+        age=n;
+    }
+    public void setname(String s){
+        name=s;
+    }
+}
+
+public class encapsul {
+    public static void main(String[] args) {
+        demo d=new demo();
+        d.setage(18);
+        d.setname("munku");
+        System.out.println(d.getage());
+        System.out.println(d.getname());
+    }
+}
