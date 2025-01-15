9ca5685ffb369a91e32fdfb34a880e4fa1c4b327
Bappa-Bairagya <bappa.09092003@gmail.com>
2025-01-15 23:21:42 +0530

    add enum

---
+++ enumnew.java
@@ -0,0 +1,12 @@
+enum color{
+    red,
+    blue,
+    green
+}
+
+public class enumnew {
+    public static void main(String[] args) {
+        color c1=color.red;
+        System.out.println(c1);
+    }
+}
