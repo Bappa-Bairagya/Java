7e8a92dc48e467f24f59ff802f0e0ffb2536f7f9
Bappa-Bairagya <bappa.09092003@gmail.com>
2025-01-20 22:59:16 +0530

    practice

--- factorial_recurson.java
+++ factorial_recurson.java
@@ -12,7 +12,7 @@ class calulate{
 public class factorial_recurson {
     public static void main(String[] args) {
         calulate cal=new calulate();
-        int result=cal.fact(9);
+        int result=cal.fact(5);
         System.out.println("Factorial is= "+result);
     }
 }
