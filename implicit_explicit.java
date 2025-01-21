513286f1102f8bd4d75b35332734a411df21b5a8
Bappa-Bairagya <bappa.09092003@gmail.com>
2025-01-21 22:29:52 +0530

    add new

--- implicit_explicit.java
+++ implicit_explicit.java
@@ -1,6 +1,6 @@
 public class implicit_explicit {
     public static void main(String[] args) {
-        System.out.println("here it's implicit");
+        System.out.println("here it's  implicit");
         int n=40;
         long l=n;
         float f=l;
